5ffcf2c4ff18cea552880782da5b31455c213d71
Cody Meadows <codyrm@gmail.com>
2023-03-14 13:16:08 -0500

    Update index (home page)

---
+++ app/scripts/location_counts.py
@@ -0,0 +1,18 @@
+from tealist.models import vendor, location, variety
+import csv
+
+
+def run():
+    Locations = location.objects.all()
+    fields = ['homelat','homelon','country','n']
+    filename = "location_counts.csv"
+
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(fields)
+
+        for Location in Locations:
+            Count = Location.tea_source.all().count()
+            if Count > 0:
+                row = ['','',Location.name,Count]
+                writer.writerow(row)
