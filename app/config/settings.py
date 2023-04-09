2ec24d98f9cf2e0825ec4a67ca98139793d0a3fc
Cody Meadows <codyrm@gmail.com>
2023-04-08 23:01:19 -0500

    Redesign

--- app/config/settings.py
+++ app/config/settings.py
@@ -118,14 +118,7 @@ else:
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 
-# DATABASES = {"default": env.db()}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {"default": env.db()}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
