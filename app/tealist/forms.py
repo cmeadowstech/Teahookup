23856b239fad0cd04d3ec1b4e264b58e4dbc74d0
Cody Meadows <codyrm@gmail.com>
2023-12-28 22:20:52 +0000

    More TW conversions

--- app/tealist/forms.py
+++ app/tealist/forms.py
@@ -7,7 +7,7 @@ class CommentForm(forms.ModelForm):
         label="",
         widget=forms.Textarea(
             attrs={
-                "class": "form-control",
+                "class": "textarea",
                 "placeholder": "Please share your thoughts...",
                 "rows": 4,
                 "cols": 50,
@@ -26,7 +26,7 @@ class VendorForm(forms.ModelForm):
         help_text="The business name of the vendor",
         widget=forms.TextInput(
             attrs={
-                "class": "form-control",
+                "class": "input",
             }
         ),
     )
@@ -34,7 +34,7 @@ class VendorForm(forms.ModelForm):
         help_text="The URL of the vendor",
         widget=forms.URLInput(
             attrs={
-                "class": "form-control",
+                "class": "input",
             }
         ),
     )
@@ -44,7 +44,7 @@ class VendorForm(forms.ModelForm):
         required=False,
         widget=forms.URLInput(
             attrs={
-                "class": "form-control",
+                "class": "input",
                 "placeholder": "Optional",
             }
         ),
@@ -53,7 +53,7 @@ class VendorForm(forms.ModelForm):
         help_text="Description of the vendor. Often pulled from about pages or Google summaries",
         widget=forms.Textarea(
             attrs={
-                "class": "form-control",
+                "class": "textarea",
                 "rows": 4,
                 "cols": 50,
             }
@@ -63,22 +63,22 @@ class VendorForm(forms.ModelForm):
         label="Ships to",
         help_text="Where does this vendor ship to?",
         queryset=location.objects.all().exclude(ship_to__isnull=True),
-        widget=forms.CheckboxSelectMultiple(),
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "peer hidden"}),
     )
     store_location = forms.ModelMultipleChoiceField(
         queryset=location.objects.all().exclude(store_location__isnull=True),
         help_text="Where does this vendor ship from?",
-        widget=forms.CheckboxSelectMultiple(),
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "peer hidden"}),
     )
     tea_source = forms.ModelMultipleChoiceField(
         help_text="Where does this vendor source its tea from?",
         queryset=location.objects.all().exclude(tea_source__isnull=True),
-        widget=forms.CheckboxSelectMultiple(),
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "peer hidden"}),
     )
     variety = forms.ModelMultipleChoiceField(
         help_text="What sort of teas does this vendor sell?",
         queryset=variety.objects.all(),
-        widget=forms.CheckboxSelectMultiple(),
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "peer hidden"}),
     )
 
     class Meta:
@@ -100,7 +100,7 @@ class CollectionForm(forms.ModelForm):
         help_text="What do you want to call this collection?",
         widget=forms.TextInput(
             attrs={
-                "class": "form-control",
+                "class": "input",
             }
         ),
     )
@@ -120,7 +120,7 @@ class CollectionForm(forms.ModelForm):
         widget=forms.Textarea(
             attrs={
                 "id": "SimpleMDE",
-                "class": "form-control",
+                "class": "textarea",
                 "placeholder": "Some additional context for this collection, if necessary.",
                 "rows": 4,
                 "cols": 50,
