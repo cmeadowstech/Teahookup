c12e4953622d96ea18805b2038c4a8b2dd0b2592
Cody Meadows <codyrm@gmail.com>
2024-02-16 20:44:52 +0000

    Update shopify_scrape to ignore compare_at_price if same as price

--- app/scripts/shopify_scrape.py
+++ app/scripts/shopify_scrape.py
@@ -80,7 +80,7 @@ def run():
                         price=Money(variant["price"], currency),
                     )
 
-                if variant["compare_at_price"] and float(variant["compare_at_price"]) > 1:
+                if variant["compare_at_price"] and float(variant["compare_at_price"]) > 1 and variant["compare_at_price"] != variant["price"]:
                     teaVariant.compare_at_price = Money(
                         variant["compare_at_price"], currency
                     )
